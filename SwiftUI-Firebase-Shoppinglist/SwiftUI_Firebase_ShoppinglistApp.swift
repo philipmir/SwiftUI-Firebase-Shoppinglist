ee2b9fbfee24fba1d3e2621cf529e02edee18d56
philipmir <philipmir@hotmail.com>
2023-04-19 14:56:54 +0200

    Initial Commit

---
+++ SwiftUI-Firebase-Shoppinglist/SwiftUI_Firebase_ShoppinglistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Firebase_ShoppinglistApp.swift
+//  SwiftUI-Firebase-Shoppinglist
+//
+//  Created by Philip Miranda on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Firebase_ShoppinglistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-Firebase-ShoppinglistUITests/SwiftUI_Firebase_ShoppinglistUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUI_Firebase_ShoppinglistUITestsLaunchTests.swift
+//  SwiftUI-Firebase-ShoppinglistUITests
+//
+//  Created by Philip Miranda on 2023-04-19.
+//
+
+import XCTest
+
+final class SwiftUI_Firebase_ShoppinglistUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

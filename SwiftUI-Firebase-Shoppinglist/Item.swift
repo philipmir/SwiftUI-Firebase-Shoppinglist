bc70f295b29a3c92bd698c9d4f715ef3b9be9aab
philipmir <philipmir@hotmail.com>
2023-05-01 15:57:09 +0200

    first commit

---
+++ SwiftUI-Firebase-Shoppinglist/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  SwiftUI-Firebase-Shoppinglist
+//
+//  Created by Philip Miranda on 2023-04-19.
+//
+
+import Foundation
---
+++ SwiftUI-Firebase-Shoppinglist/ShoppingListVM.swift
@@ -0,0 +1,8 @@
+//
+//  ShoppingListVM.swift
+//  SwiftUI-Firebase-Shoppinglist
+//
+//  Created by Philip Miranda on 2023-04-20.
+//
+
+import Foundation
